1196076d3c1ededdbb203537aef5ca9a8e47d240
seanposton4 <sean.poston4@gmail.com>
2021-09-02 19:17:09 -0500

    IndivAssign1.py

--- CS350/IndivAssign1.py
+++ CS350/IndivAssign1.py
@@ -1,13 +1,25 @@
 def decode(encoded:str) -> str:
   stack = []
-  decoded = ''
+  stack.append(['', 1])
+  times = ''
   for i in encoded:
-    if (i == '['):
-  return decoded
+    if i.isdigit():
+      times += i
+    elif i == '[':
+      stack.append(['', int(times)])
+      times = ''
+    elif i == ']':
+      s, t = stack.pop()
+      stack[-1][0] += s*t
+    else:
+      stack[-1][0] += i
+  return stack [0][0]
+
+    
 
 if __name__ == '__main__':
   encodedExamples = ['2[x]3[mn]', '2[z3[x]]']
   #Should output 'xxmnmnmn' and 'zxxxzxxx'
 
   print(decode(encodedExamples[0]))
-  #print(decode(encodedExamples[1]))
+  print(decode(encodedExamples[1]))
