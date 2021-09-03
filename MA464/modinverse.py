d2eb930ac3b53b5391fa766b118b7f76d365baf6
seanposton4 <sean.poston4@gmail.com>
2021-09-02 21:44:12 -0500

    made more concise

--- MA464/modinverse.py
+++ MA464/modinverse.py
@@ -1,19 +1,19 @@
 import math
 mod = 8
 
-invertibleAndInverses = {'Invertible Elems': [], 'Inverses': []}
+invertibleAndInverses = {'Invertible': [], 'Inverse': []}
 for i in range(mod):
     if math.gcd(i, mod) == 1:
-        invertibleAndInverses['Invertible Elems'].append(i)
+        invertibleAndInverses['Invertible'].append(i)
         for j in range(mod):
             if (i * j) % mod == 1:
-                invertibleAndInverses['Inverses'].append(j)
+                invertibleAndInverses['Inverse'].append(j)
 
-print(f'Invertibles: {invertibleAndInverses["Invertible Elems"]}')
-print(f'Inverses:    {invertibleAndInverses["Inverses"]}')
+print(f'Invertibles: {invertibleAndInverses["Invertible"]}')
+print(f'Inverses:    {invertibleAndInverses["Inverse"]}')
 
-# #Uncomment to write to a file if output is too long.
+#Uncomment to write to a file if output is too long.
 # fileout = open('output.txt', 'w')
 # fileout.write(f'Modulo: {mod}\n\n')
-# fileout.write(f'Invertibles: {str(invertibleAndInverses["Invertible Elems"])}')
-# fileout.write(f'\nInverses:    {str(invertibleAndInverses["Inverses"])}')
+# fileout.write(f'Invertibles: {str(invertibleAndInverses["Invertible"])}')
+# fileout.write(f'\nInverses:    {str(invertibleAndInverses["Inverse"])}')
