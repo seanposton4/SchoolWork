38372417690a34506633f4d451f3450b845015f6
seanposton4 <sean.poston4@gmail.com>
2021-09-02 20:28:54 -0500

    added comments

--- CS350/IndivAssign1.py
+++ CS350/IndivAssign1.py
@@ -6,26 +6,29 @@ def decode(encoded:str) -> str:
   for i in encoded:
     # This if statement will grab the times to repeat the number.
     # It will be in the range [1, 300], so this will cover the 
-    #  case of multiple numbers.
+    # case of multiple numbers.
     if i.isdigit():
       times = (times * 10) + int(i)
     elif i == '[':
-      stack.append(decoded)
+    # Adds current repetitions, times, and current value of decoded
+    # to the stack to be popped on ']'.
       stack.append(times)
+      stack.append(decoded)
       decoded = ''
       times = 0
+    # Pops the rightmost value, times, then pops the previous 
+    # string to be added on to the string holder.
     elif i == ']':
-      reps = stack.pop()
-      prevStr = stack.pop()
-      decoded = prevStr + (reps * decoded)
+      decoded = stack.pop() + (decoded * stack.pop())
+    # Adds message to decoded character by character.
     else:
       decoded += i
     
   return decoded
 
 if __name__ == '__main__':
-  encodedExamples = ['2[x]3[mn]', '2[z3[x]]']
+  encodedExamples = ['2[x]3[mn]', '2[z3[x]]', '3[ab2[yy2[x]]]']
   #Should output 'xxmnmnmn' and 'zxxxzxxx'
 
-  print(decode(encodedExamples[0]))
-  print(decode(encodedExamples[1]))
+  for i in range(len(encodedExamples)):
+    print(decode(encodedExamples[i]))
