925f36c2ebaec40596078418474b20a6a399179d
seanposton4 <sean.poston4@gmail.com>
2022-04-09 16:58:16 -0500

    add some stuff pt 1

---
+++ COBOL/Quicksort/quicksortfunc.cob
@@ -0,0 +1,38 @@
+       >>SOURCE FORMAT FREE
+IDENTIFICATION DIVISION.
+PROGRAM-ID. quicksortfunc RECURSIVE.
+AUTHOR.     Sean Poston.
+DATA DIVISION.
+LINKAGE SECTION.
+       01 LIST.
+           02 ARRAYINDEX PIC 9(3) OCCURS 10 TIMES.
+       01 SORTVARS.
+           02 I            PIC 9(2).
+           02 USERINPUT    PIC 9(2).
+           02 FIRST1       PIC 9(2).
+           02 LAST1        PIC 9(2). 
+       01 PARTITIONVARS.
+           02 PIVOT        PIC 9(2).
+           02 LOW1         PIC 9(2).
+           02 HIGH1        PIC 9(2).
+       01 SWAPVARS.
+           02 TEMP         PIC 9(2).
+           02 INDEXA       PIC 9(2).
+           02 INDEXB       PIC 9(2).
+       01 RETURNVARS.
+           02 PIVOTINDEX   PIC 9(2).
+
+PROCEDURE DIVISION USING LIST, SORTVARS, PARTITIONVARS, SWAPVARS, RETURNVARS.
+       IF LAST1 > FIRST1 THEN
+      *> Get PIVOTINDEX for current iteration.
+           CALL 'partition' USING LIST, BY CONTENT SORTVARS, BY CONTENT PARTITIONVARS, BY CONTENT SWAPVARS, RETURNVARS
+
+           SUBTRACT 1 FROM PIVOTINDEX
+           CALL 'quicksortfunc' USING LIST, BY CONTENT SORTVARS, BY CONTENT SWAPVARS, RETURNVARS
+
+           ADD 2 TO PIVOTINDEX
+           CALL 'quicksortfunc' USING LIST, BY CONTENT SORTVARS, BY CONTENT SWAPVARS, RETURNVARS
+
+           SUBTRACT 1 FROM PIVOTINDEX
+       END-IF.
+EXIT PROGRAM.
